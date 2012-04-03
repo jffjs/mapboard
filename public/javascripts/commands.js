e1eee441593b757f824f4da0c4152ca3b7e5b818
Jeff Smith <jffreyjs@gmail.com>
2012-04-02 21:45:50 -0400

    changed commands from functions to objects in order to create documentation strings for /help

--- public/javascripts/commands.js
+++ public/javascripts/commands.js
@@ -1,35 +1,47 @@
 define(["./dispatch"], function(dispatch) {
   var commands = {
-    help: function() {
-      var msg = "/go <location> - change map center to location"
-      dispatch.to("chat", { text: msg,
-                            type: "command" });
-    },
-    go: function(args) {
-      var geocoder = new google.maps.Geocoder();
-      var address = args.join(' ');
-      geocoder.geocode({ address: address }, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-          var center = results[0].geometry.location;
-          dispatch.to("map", { center: { lat: center.lat(),
-                                         lng: center.lng() }
-          });
+    help: {
+      doc: "",
+      fn: function() {
+        var msg;
+        for(var c in commands) {
+          msg = commands[c].doc
+          dispatch.to("chat", { text: msg,
+                                type: "command" });
         }
-      });
+      }
     },
-    zoom: function(args) {
-      var zoom = parseInt(args[0]);
-      if (isNaN(zoom)) {
-        dispatch.to("chat", { text: "You must specify an integer to zoom to.",
-                              type: "command" });
-      } else {
-        dispatch.to("map", { zoom: zoom });
+    go: {
+      doc: "/go <location> - change map center to location",
+      fn: function(args) {
+        var geocoder = new google.maps.Geocoder();
+        var address = args.join(' ');
+        geocoder.geocode({ address: address }, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            var center = results[0].geometry.location;
+            dispatch.to("map", { center: { lat: center.lat(),
+                                           lng: center.lng() }
+            });
+          }
+        });
+      }
+    },
+    zoom: {
+      doc: "/zoom <number> - set the zoom level, must be an integer",
+      fn: function(args) {
+        var zoom = parseInt(args[0]);
+        if (isNaN(zoom)) {
+          dispatch.to("chat", { text: "You must specify an integer to zoom to.",
+                                type: "command" });
+        } else {
+          dispatch.to("map", { zoom: zoom });
+        }
       }
     }
   }
 
   var run = function(command, args) {
-    commands[command](args);
+    commands[command].fn(args);
   }
 
   return {
